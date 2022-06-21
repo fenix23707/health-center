50761c13d90a57807d277ee3efa1039fc7d1921a
Vladislav Kovzov <fenix23707@gmail.com>
2022-06-21 14:42:56 +0300

    added sql

---
+++ sql/init.sql
@@ -0,0 +1,12 @@
+
+DROP TABLE users;
+
+CREATE TABLE users
+(
+    PRIMARY KEY (id),
+    id       serial,
+    login    varchar(255) NOT NULL UNIQUE,
+    password varchar(100) NOT NULL,
+    role     smallint     NOT NULL DEFAULT 0,
+    status   smallint     NOT NULL DEFAULT 0
+);
