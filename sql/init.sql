2c895d26c8d30589f054a242fb1517873ca6d006
Vladislav Kovzov <fenix23707@gmail.com>
2022-06-22 14:21:14 +0300

    update sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,4 +1,3 @@
-
 DROP TABLE users;
 
 CREATE TABLE users
@@ -7,11 +6,45 @@ CREATE TABLE users
     id       serial,
     login    varchar(255) NOT NULL UNIQUE,
     password varchar(100) NOT NULL,
-    role     smallint     NOT NULL DEFAULT 0,
-    status   smallint     NOT NULL DEFAULT 0
+    role     smallint     NOT NULL DEFAULT 0
 );
 
-SELECT * FROM users;
+CREATE TABLE specializations
+(
+    id        serial       NOT NULL,
+    name      VARCHAR(255) NOT NULL UNIQUE,
+    wage_rate FLOAT        NOT NULL UNIQUE,
+    narrow    BOOLEAN      NOT NULL,
+    CONSTRAINT specializations_pk PRIMARY KEY ("id")
+);
+
+CREATE TABLE doctor
+(
+    PRIMARY KEY (id),
+    id                  serial,
+    "employment_date"   DATE    NOT NULL,
+    "salary"            FLOAT   NOT NULL,
+    "person_id"         integer NOT NULL,
+    "specialization_id" integer NOT NULL,
+    "branch_id"         integer NOT NULL
+);
+
+CREATE TABLE person
+(
+    PRIMARY KEY (id),
+    id           serial,
+    "name"       VARCHAR(255) NOT NULL,
+    "surname"    VARCHAR(255) NOT NULL,
+    "patronymic" VARCHAR(255) NOT NULL,
+    "sex"        integer      NOT NULL,
+    "dob"        DATE         NOT NULL
+);
+
+ALTER TABLE "doctor"
+    ADD CONSTRAINT "doctor_fk0" FOREIGN KEY ("person_id") REFERENCES "person" ("id");
+ALTER TABLE "doctor"
+    ADD CONSTRAINT "doctor_fk1" FOREIGN KEY ("specialization_id") REFERENCES "specializations" ("id");
+
 
 INSERT INTO users(login, password)
 VALUES ('admin', 'admin');
